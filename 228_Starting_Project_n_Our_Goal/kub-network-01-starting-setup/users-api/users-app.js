a3269ec0b577112b4d0c030a25544907c277c9f8
Hielapha.Jackpot <indianapacer15@hotmail.com>
2025-10-11 17:20:37 +0800

    practices: change k8s config   - make individual deployment and service files for auth api   - change networking methods through k8s built-in dns service discovery and env vars injection

--- 228_Starting_Project_n_Our_Goal/kub-network-01-starting-setup/users-api/users-app.js
+++ 228_Starting_Project_n_Our_Goal/kub-network-01-starting-setup/users-api/users-app.js
@@ -23,11 +23,17 @@ app.post('/signup', async (req, res) => {
   }
 
   try {
-    // 當 users service 的 k8s config 設定完成，並且有實際連至 auth service 時，就可以用下面這行，並且將 auth-api 的 k8s 相關設定建置好
-    // 在沒有使用 k8s 建置前，可以直接使用 docker-compose 的 service name 當作 hostname，
-    const hashedPW = await axios.get(`http://${process.env.AUTH_ADDRESS}/hashed-password/` + password);
     // const hashedPW = 'dummy text'; // 只有 users service 時先用 dummy text 代替，目前 demo 暫時不發真的 request 到 auth service
     // since it's a dummy service, we don't really care for the hashed-pw either
+
+    // 當 users service 的 k8s config 設定完成，並且有實際連至 auth service 時，就可以用下面這行，並且將 auth-api 的 k8s 相關設定建置好
+    // 在沒有使用 k8s 建置前，可以直接使用 docker-compose 的 service name 當作 hostname
+    // const hashedPW = await axios.get(`http://${process.env.AUTH_ADDRESS}/hashed-password/` + password);
+    
+    // 當建置將 auth-api 獨立成一個 pod 之後，就需要用 service name 當作 hostname
+    // 此時 k8s 提供了一個方便的 pattern，只要將宣告好 service name 做適當的變更即可，這個是 k8s 自動注入的環境變數
+    // e.g. auth-service 變成 AUTH_SERVICE_SERVICE_HOST
+    const hashedPW = await axios.get(`http://${process.env.AUTH_SERVICE_SERVICE_HOST}/hashed-password/` + password);
     console.log(hashedPW, email);
     res.status(201).json({ message: 'User created!' });
   } catch (err) {
